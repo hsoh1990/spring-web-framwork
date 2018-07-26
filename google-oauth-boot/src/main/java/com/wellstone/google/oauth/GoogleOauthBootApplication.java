e44525636f10a4e17e3c73e75b1153decb9c0cfc
wellstone <ohs4123@gmail.com>
2018-07-26 17:32:14 +0900

    oauth 기본 동작 구현

---
+++ google-oauth-boot/src/main/java/com/wellstone/google/oauth/GoogleOauthBootApplication.java
@@ -0,0 +1,17 @@
+package com.wellstone.google.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class GoogleOauthBootApplication {
+
+    private static final String PROPERTIES = "spring.config.location=classpath:/google.yml";
+
+	public static void main(String[] args) {
+	    new SpringApplicationBuilder(GoogleOauthBootApplication.class)
+                .properties(PROPERTIES)
+                .run(args);
+	}
+}
