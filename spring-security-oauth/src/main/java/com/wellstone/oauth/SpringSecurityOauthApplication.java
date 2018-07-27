6ea26cf954b28d7574e21e4560aaeea2a606281a
wellstone <ohs4123@gmail.com>
2018-07-27 11:32:55 +0900

    google OAuth2 인증 기본 설정

---
+++ spring-security-oauth/src/main/java/com/wellstone/oauth/SpringSecurityOauthApplication.java
@@ -0,0 +1,17 @@
+package com.wellstone.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class SpringSecurityOauthApplication {
+
+    private static final String PROPERTIES = "spring.config.location=classpath:/google.yml";
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(SpringSecurityOauthApplication.class)
+                .properties(PROPERTIES)
+                .run(args);
+    }
+}
