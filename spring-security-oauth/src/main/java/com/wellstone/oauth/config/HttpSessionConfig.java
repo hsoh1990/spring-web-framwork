de2862012b65d44119c10f2aee8bcd4955456ac9
wellstone <ohs4123@gmail.com>
2018-07-27 11:36:01 +0900

    google OAuth2 h2 session 설정

---
+++ spring-security-oauth/src/main/java/com/wellstone/oauth/config/HttpSessionConfig.java
@@ -0,0 +1,7 @@
+package com.wellstone.oauth.config;
+
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+
+@EnableJdbcHttpSession
+public class HttpSessionConfig {
+}
