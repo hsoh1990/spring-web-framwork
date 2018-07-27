7658b42f1e26d3591c0d6bca7f26cd9e439d4982
wellstone <ohs4123@gmail.com>
2018-07-27 13:46:10 +0900

    application 설정 변경 추후예정인 facebook, kakao 추가

---
+++ spring-security-oauth/src/main/java/com/wellstone/oauth/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.wellstone.oauth.config;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public ObjectMapper objectMapper(){
+        return new ObjectMapper();
+    }
+}
